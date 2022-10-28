3143dc9483564c9c8650b4cad3ca989003e693b5
Monochrome <zhaoyangmushiyi@gmail.com>
2022-10-28 20:20:43 +0800

    更新支持自定义规则的灰度发布组件-MVP版，移除不需要的方法.

--- dark-launch/src/main/java/com/monochrome/darklaunch/DarkLaunch.java
+++ dark-launch/src/main/java/com/monochrome/darklaunch/DarkLaunch.java
@@ -45,36 +45,6 @@ public class DarkLaunch {
         this(DEFAULT_RULE_UPDATE_TIME_INTERVAL);
     }
 
-    private void loadRule() {
-        // 将灰度规则配置文件dark-rule.yaml中的内容读取DarkRuleConfig中
-        InputStream in = null;
-        DarkRuleConfig ruleConfig = null;
-        try {
-            in = this.getClass().getResourceAsStream("/dark-rule.yaml");
-            if (in != null) {
-                Yaml yaml = new Yaml();
-                ruleConfig = yaml.loadAs(in, DarkRuleConfig.class);
-            }
-        } finally {
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (IOException e) {
-                    log.error("close file error:{}", e);
-                }
-            }
-        }
-
-        if (ruleConfig == null) {
-            throw new RuntimeException("Can not load dark rule.");
-        }
-        // 更新规则并非直接在this.rule上进行，
-        // 而是通过创建一个新的DarkRule，然后赋值给this.rule，
-        // 来避免更新规则和规则查询的并发冲突问题
-        DarkRule newRule = new DarkRule(ruleConfig);
-        this.rule = newRule;
-    }
-
     public DarkFeature getDarkFeature(String featureKey) {
         DarkFeature darkFeature = this.rule.getDarkFeature(featureKey);
         return darkFeature;
